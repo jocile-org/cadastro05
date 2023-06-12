10ceadfdbf48b4acad208f39b147a1875930a0a8
jocile <jocile@gmail.com>
2023-06-12 20:51:48 -0300

    inicio

---
+++ src/main/java/com/jocile/cadastro05/Cadastro05.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jocile.cadastro05;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cadastro05 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
